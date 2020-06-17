e960ef8ce06ea0c496777220f103ad0d489c86ff
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-06-17 10:15:54 -0300

    Enumeracao OrderStatus

--- src/main/java/com/jeangabriel/course/entities/Order.java
+++ src/main/java/com/jeangabriel/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jeangabriel.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -30,12 +31,15 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	private Integer orderStatus;
+
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -61,6 +65,15 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
 	}
 
 	@Override
